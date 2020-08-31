000e122d06cacaf3b172334ec3da78a46b6e4dad
Swadhin Paul <swadhinpaul159@gmail.com>
2020-09-01 00:15:58 +0530

    styles added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import { robots } from "./robots";
 import "tachyons";
+import "./App.css";
 
 class App extends React.Component {
   constructor() {
@@ -25,7 +26,7 @@ class App extends React.Component {
     });
     return (
       <div className="tc">
-        <h1>RoboFriends</h1>
+        <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredroboat} />
       </div>
